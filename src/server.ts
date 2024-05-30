a3380df6deb20f37f98c4ef38bd7ccc675ac60d3
Kareem Amara <kareemamarah2356@gmail.com>
2024-05-30 19:19:31 +0300

    Update a product - PATCH Method

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, {Request, Response} from "express";
 import { generateFakeProducts } from "./utils/fakeData";
+import { Product } from "./interfaces";
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (_req, res)=> {
     res.send(`<h1>Hello Express.js</h1>`)
@@ -10,21 +13,82 @@ app.get('/', (_req, res)=> {
 const fakeProductsData = generateFakeProducts();
 // Endpoints (PRODUCTS)
 app.get('/products', (req, res)=> {
-    res.send(fakeProductsData)
+    // Filter By, keyof Product
+    const filterQuery = req.query.filter as string;
+    if (filterQuery) {
+        const propertiesToFilter = filterQuery.split(',');
+        let filteredProducts = []
+        filteredProducts = fakeProductsData.map(product => {
+            const filteredProduct: any = {};
+            propertiesToFilter.forEach(property => {
+                if (product.hasOwnProperty(property as keyof Product)){
+                   filteredProduct[property] = product[property as keyof Product] 
+                } 
+            })
+            return {id:product.id,...filteredProduct};
+        })
+        return res.send(filteredProducts);
+    }
+   return res.send(fakeProductsData)
 })
+
 app.get('/products/:id', (req: Request, res: Response)=> {
     console.log(+req.params.id)
     const productId = +req.params.id;
     if (isNaN(productId)) {
          res.status(404).send({message:"Invalid product ID"})
     }
-    const findProduct:{id: number, title: string, price: number} | undefined = fakeProductsData.find((product) => product.id === productId);
+    const findProduct: Product | undefined = fakeProductsData.find((product) => product.id === productId);
     if (findProduct) {
         res.send({id:`${productId}`, name:findProduct.title, price:findProduct.price });
     } else {
         res.status(404).send({message:"Product not found"})
 
     }
+})
+
+// Create a new product
+app.post('/products', (req, res) => {
+    const newProduct = req.body;
+
+    fakeProductsData.push({
+        id: fakeProductsData.length + 1,
+        ...newProduct
+    })
+
+    res.status(201).send({
+        id: fakeProductsData.length + 1,
+        title: newProduct.title,
+        price: newProduct.price,
+        description: newProduct.description
+    })
+})
+
+// UPDATE
+app.patch('/products/:id', (req, res)=> {
+    const productId = +req.params.id;
+    if(isNaN(productId)){
+        return res.status(404).send({
+            message: "Product not found!"
+        })
+    }
+ const productIndex : number | undefined = fakeProductsData.findIndex(product => product.id === productId);
+ const productBody = req.body;
+ if(productIndex !== -1){
+    fakeProductsData[productIndex] = {...fakeProductsData[productIndex],...productBody}; 
+    return res.status(200).send({
+        message: "Product has been updated!"
+    })
+ } else{
+     return res.status(404).send({
+        message: "Product not found!"
+     })
+
+ }
+
+ 
+
+
 })
 
 
