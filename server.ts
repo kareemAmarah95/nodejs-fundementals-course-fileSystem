65d50fe917bd813f600c7dacb0e9eb2b1e30de00
Kareem Amara <kareemamarah2356@gmail.com>
2024-06-20 21:56:19 +0300

    Rate Limit and Compression Middlewares

--- server.ts
+++ server.ts
@@ -1,4 +1,6 @@
 import express, {Request, Response} from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 import path from "path";
 import { generateFakeProducts } from './utils/fakeData';
 import ProductService from './services/ProductService';
@@ -8,10 +10,32 @@ import ProductsViewController from './controllers/ProductsViewController';
 import ErrorMiddleware from './middlewares/Error';
 import dotenv from "dotenv";
 import NotFoundMiddleware from './middlewares/NotFound';
+import  rateLimit  from 'express-rate-limit'
+import compression from "compression";
 const app = express();
-
-app.use(express.json());
 dotenv.config();
+
+const rateLimiterOptions = {
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	message: "Too many requests from this IP, please try again later."
+}
+
+app.use(compression());
+app.use(express.json());
+app.use(rateLimit(rateLimiterOptions))
+
+// Use Helmet!
+app.use(helmet({
+  // ! Danger : Don't write this line in PRODUCTION  
+  contentSecurityPolicy: false,
+  xFrameOptions : {
+    action: "deny" 
+  }
+}));
+
+app.use(morgan('dev'))
+
 app.set('view engine', 'pug');
 
 app.set('views', path.join(__dirname,"views"))
